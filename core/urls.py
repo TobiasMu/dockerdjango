cc7d8d068a5a4af2e082a0c576c622560f5e82dc
TobiasMu <64594392+TobiasMu@users.noreply.github.com>
2024-10-29 06:57:26 +0100

    fix mistake

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,6 @@ app_name = "core"
 urlpatterns = [
     path("create_trade/", CreateTrade.as_view(), name="createtrade"),
     path("create_product/", CreateProduct.as_view(), name="createproduct"),
-    path("list_trade/", ListProducts.as_view(), name="listtrades"),
-    path("list_product/", ListTrades.as_view(), name="listproducts"),
+    path("list_trade/", ListProducts.as_view(), name="listproducts"),
+    path("list_product/", ListTrades.as_view(), name="listtrades"),
 ]
